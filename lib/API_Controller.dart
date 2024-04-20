dde03907f8e04010a9643893334dfc6991a7e75f
KrisTheMuggle <kris.themuggle@gmail.com>
2024-04-20 19:21:08 +1000

    Add a basic API_Controller file

---
+++ lib/API_Controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class APIController extends StatefulWidget {
+  const APIController({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _APIController();
+  }
+}
+
+class _APIController extends State<APIController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Placeholder"),
+    );
+  }
+}
