5085a75664b9102ed1ba5c5fd317e5ed7636ae2a
KrisTheMuggle <kris.themuggle@gmail.com>
2024-04-21 10:25:02 +1000

    Add an update to display fetched data

--- lib/API_Controller.dart
+++ lib/API_Controller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:networking_test/Album.dart';
+import 'package:networking_test/Request.dart';
 
 class APIController extends StatefulWidget {
   const APIController({super.key});
@@ -10,10 +12,40 @@ class APIController extends StatefulWidget {
 }
 
 class _APIController extends State<APIController> {
+  late Future<Album> futureAlbum;
+  late final MakeRquest api = MakeRquest();
+  void initState() {
+    super.initState();
+    futureAlbum = api.fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Placeholder"),
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
